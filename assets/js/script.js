aa67c4b340d24c1d3b2e145a82a5e48ec807a76f
Matheus Meigre <142261154+matheusmeigre@users.noreply.github.com>
2024-03-28 13:35:48 -0300

    test finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,77 @@
 // Initial Data
-let currentQuestion = 0
-let correctAnswers = 0
-showQuestion()
+let currentQuestion = 0;
+let correctAnswers = 0;
+
+showQuestion();
+
 // Events
-document.querySelector('.scoreArea button').addEventListener('click', resetEvent)
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
 
 // Functions
 function showQuestion() {
     if (questions[currentQuestion]) {
-        let q = questions[currentQuestion]
+        let q = questions[currentQuestion];
 
-        let pct = Math.floor((currentQuestion / questions.length) * 100)
+        let pct = Math.floor((currentQuestion / questions.length) * 100);
 
-        document.querySelector('.progress--bar').style.width = `${pct}%`
-        document.querySelector('.scoreArea').style.display = 'none'
-        document.querySelector('.questionArea').style.display = 'block'
+        document.querySelector('.progress--bar').style.width = `${pct}%`;
+        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.questionArea').style.display = 'block';
 
-        document.querySelector('.question').innerHTML = q.question
-        document.querySelector('.options').innerHTML = ''
-        let optionsHtml = ''
+        document.querySelector('.question').innerHTML = q.question;
+        document.querySelector('.options').innerHTML = '';
+        let optionsHtml = '';
         for (let i in q.options) {
-            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i) + 1}</span>${q.options[i]}</div>`
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i) + 1}</span>${q.options[i]}</div>`;
         }
-        document.querySelector('.options').innerHTML = optionsHtml
+        document.querySelector('.options').innerHTML = optionsHtml;
 
         document.querySelectorAll('.options .option').forEach(item => {
-            item.addEventListener('click', optionClickEvent)
-        })
+            item.addEventListener('click', optionClickEvent);
+        });
     } else {
-        finishedQuiz()
+        finishedQuiz();
     }
 }
 
 function optionClickEvent(e) {
-    let clickedOption = parseInt(e.target.getAttribute('data-op'))
+    let clickedOption = parseInt(e.target.getAttribute('data-op'));
 
-    if(questions[currentQuestion].answer === clickedOption) {
-        correctAnswers++
+    if (questions[currentQuestion].answer === clickedOption) {
+        correctAnswers++;
     }
 
-    currentQuestion++
-    showQuestion()
+    currentQuestion++;
+    showQuestion();
 }
 
 function finishedQuiz() {
-    let points = Math.floor((correctAnswers / questions.length) * 100)
+    let points = Math.floor((correctAnswers / questions.length) * 100);
 
-    if(points < 30) {
-        document.querySelector('.scoreText1').innerHTML = 'Precisa melhorar!'
-        document.querySelector('.scorePct').style.color = '#FF0000'
-    } else if(points >= 30 && points < 70) {
-        document.querySelector('.scoreText1').innerHTML = 'Bom!'
-        document.querySelector('.scorePct').style.color = '#FF0000'
-     } else if(points >= 70) {
-        document.querySelector('.scoreText1').innerHTML = 'Parabéns!'
-        document.querySelector('.scorePct').style.color = '#0D630D'
-     }
+    if (points < 30) {
+        document.querySelector('.scoreText1').innerHTML = 'Precisa melhorar!';
+        document.querySelector('.scorePct').style.color = '#FF0000';
+    } else if (points >= 30 && points < 70) {
+        document.querySelector('.scoreText1').innerHTML = 'Bom!';
+        document.querySelector('.scorePct').style.color = '#FF0000';
+    } else if (points >= 70) {
+        document.querySelector('.scoreText1').innerHTML = 'Parabéns!';
+        document.querySelector('.scorePct').style.color = '#0D630D';
+    }
 
-    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`
-    document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} e acertou ${correctAnswers}`
+    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`;
+    document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} e acertou ${correctAnswers}`;
 
-    document.querySelector('.scoreArea').style.display = 'block'
-    document.querySelector('.questionArea').style.display = 'none'
-    document.querySelector('.progress--bar').style.width = '100%'
-
+    document.querySelector('.scoreArea').style.display = 'block';
+    document.querySelector('.questionArea').style.display = 'none';
+    document.querySelector('.progress--bar').style.width = '100%';
 }
 
 function resetEvent() {
-    correctAnswers = 0
-    currentQuestion = 0
-    questions = shuffleArray(questions)
-    showQuestion()
+    correctAnswers = 0;
+    currentQuestion = 0;
+    questions = shuffleArray(questions);
+    showQuestion();
 }
 
 function shuffleArray(array) {
